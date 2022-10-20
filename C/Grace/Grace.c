11e95dbeec499d06c3c90173aff446928da5a3ff
mamartin <mamartin@student.42.fr>
2022-10-20 00:08:17 -0400

    last fixes before push

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#define SRC_CODE "#include <stdio.h>%1$c%1$c#define SRC_CODE %3$c%4$s%3$c%1$c#define NOMAIN(a, b, x, X, e, f) int a##b##e##f() { FILE* file = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c); if (file) { fprintf(file, SRC_CODE, 10, 9, 34, SRC_CODE); fclose(file); } }%1$c#define START() NOMAIN(m, a, X, X, i, n)%1$c%1$c/*%1$c%2$cEntrypoint of the program%1$c*/%1$cSTART()%1$c"
-#define NOMAIN(a, b, x, X, e, f) int a##b##e##f() { FILE* file = fopen("Grace_kid.c", "w"); if (file) { fprintf(file, SRC_CODE, 10, 9, 34, SRC_CODE); fclose(file); } }
-#define START() NOMAIN(m, a, X, X, i, n)
+#define SOURCE "#include <stdio.h>%1$c%1$c#define SOURCE %3$c%4$s%3$c%1$c#define CODE FILE* file = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c); if (file) { fprintf(file, SOURCE, 10, 9, 34, SOURCE); fclose(file); }%1$c#define FT(x) int main() { CODE }%1$c%1$c/*%1$c%2$cEntrypoint of the program%1$c*/%1$cFT(xxxxxxxx)%1$c"
+#define CODE FILE* file = fopen("Grace_kid.c", "w"); if (file) { fprintf(file, SOURCE, 10, 9, 34, SOURCE); fclose(file); }
+#define FT(x) int main() { CODE }
 
 /*
 	Entrypoint of the program
 */
-START()
+FT(xxxxxxxx)
