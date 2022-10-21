6aafa120e6098a37eca652ee0f267fb51b2f025c
mamartin <mamartin@student.42.fr>
2022-10-20 22:10:54 -0400

    fixes for retry

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -14,6 +14,7 @@ sub rsp, 16
 mov rax, 0x2000005
 mov rdi, filename
 mov rsi, 0x201
+mov rdx, 0x180
 syscall
 jc syscall_error
 mov [rbp - 4], rax
@@ -45,9 +46,10 @@ mov rax, 1
 jmp exit
 %endmacro
 
+; entrypoint
 START
 
 section .data
 FILENAME
 ERRMSG
-code: db "section .text%1$cextern _dprintf%1$c%1$c%%define FILENAME filename: db %2$cGrace_kid.s%2$c,0x0%1$c%%define ERRMSG errmsg: db %2$cFailed to open %%s%2$c,0xa,0x0%1$c%1$c%%macro START 0%1$cglobal _main%1$c_main:%1$cpush rbp%1$cmov rbp, rsp%1$csub rsp, 16%1$c%1$cmov rax, 0x2000005%1$cmov rdi, filename%1$cmov rsi, 0x201%1$csyscall%1$cjc syscall_error%1$cmov [rbp - 4], rax%1$c%1$cmov rdi, [rbp - 4]%1$cmov rsi, code%1$cmov rdx, 0xa%1$cmov rcx, 0x22%1$cmov r8, code%1$ccall _dprintf%1$c%1$cmov rax, 0x2000006%1$cmov rdi, [rbp - 4]%1$csyscall%1$c%1$cxor rax, rax%1$cexit:%1$cmov rsp, rbp%1$cpop rbp%1$cret%1$c%1$csyscall_error:%1$cmov rdi, 0x2%1$cmov rsi, errmsg%1$cmov rdx, filename%1$cmov rax, 0%1$ccall _dprintf%1$cmov rax, 1%1$cjmp exit%1$c%%endmacro%1$c%1$cSTART%1$c%1$csection .data%1$cFILENAME%1$cERRMSG%1$ccode: db %2$c%3$s%2$c,0x0%1$c",0x0
+code: db "section .text%1$cextern _dprintf%1$c%1$c%%define FILENAME filename: db %2$cGrace_kid.s%2$c,0x0%1$c%%define ERRMSG errmsg: db %2$cFailed to open %%s%2$c,0xa,0x0%1$c%1$c%%macro START 0%1$cglobal _main%1$c_main:%1$cpush rbp%1$cmov rbp, rsp%1$csub rsp, 16%1$c%1$cmov rax, 0x2000005%1$cmov rdi, filename%1$cmov rsi, 0x201%1$cmov rdx, 0x180%1$csyscall%1$cjc syscall_error%1$cmov [rbp - 4], rax%1$c%1$cmov rdi, [rbp - 4]%1$cmov rsi, code%1$cmov rdx, 0xa%1$cmov rcx, 0x22%1$cmov r8, code%1$ccall _dprintf%1$c%1$cmov rax, 0x2000006%1$cmov rdi, [rbp - 4]%1$csyscall%1$c%1$cxor rax, rax%1$cexit:%1$cmov rsp, rbp%1$cpop rbp%1$cret%1$c%1$csyscall_error:%1$cmov rdi, 0x2%1$cmov rsi, errmsg%1$cmov rdx, filename%1$cmov rax, 0%1$ccall _dprintf%1$cmov rax, 1%1$cjmp exit%1$c%%endmacro%1$c%1$c; entrypoint%1$cSTART%1$c%1$csection .data%1$cFILENAME%1$cERRMSG%1$ccode: db %2$c%3$s%2$c,0x0%1$c",0x0
--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -3,8 +3,13 @@
 
 int main()
 {
-	int i = 5;
+	int i = -1;
+	if (i < 0)
+		return 0;
+
+#ifdef REPLICATE
 	i--;
+#endif
 
 	char filename[10];
 	sprintf(filename, "Sully_%d.c", i);
@@ -13,14 +18,14 @@ int main()
 	if (!outfd)
 		return 1;
 
-	const char* source = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$cint main()%1$c{%1$c%2$cint i = %5$d;%1$c%2$ci--;%1$c%1$c%2$cchar filename[10];%1$c%2$csprintf(filename, %3$cSully_%%d.c%3$c, i);%1$c%1$c%2$cFILE* outfd = fopen(filename, %3$cw%3$c);%1$c%2$cif (!outfd)%1$c%2$c%2$creturn 1;%1$c%1$c%2$cconst char* source = %3$c%4$s%3$c;%1$c%2$cfprintf(outfd, source, 10, 9, 34, source, i);%1$c%2$cfclose(outfd);%1$c%1$c%2$cchar command[100];%1$c%2$csprintf(command, %3$cgcc %%1$s -o Sully_%%2$d%3$c, filename, i);%1$c%2$csystem(command);%1$c%2$cif (i)%1$c%2$c{%1$c%2$c%2$csprintf(command, %3$c./Sully_%%d%3$c, i);%1$c%2$c%2$csystem(command);%1$c%2$c}%1$c%2$creturn 0;%1$c}%1$c";
+	const char* source = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$cint main()%1$c{%1$c%2$cint i = %5$d;%1$c%2$cif (i < 0)%1$c%2$c%2$creturn 0;%1$c%1$c#ifdef REPLICATE%1$c%2$ci--;%1$c#endif%1$c%1$c%2$cchar filename[10];%1$c%2$csprintf(filename, %3$cSully_%%d.c%3$c, i);%1$c%1$c%2$cFILE* outfd = fopen(filename, %3$cw%3$c);%1$c%2$cif (!outfd)%1$c%2$c%2$creturn 1;%1$c%1$c%2$cconst char* source = %3$c%4$s%3$c;%1$c%2$cfprintf(outfd, source, 10, 9, 34, source, i);%1$c%2$cfclose(outfd);%1$c%1$c%2$cchar command[100];%1$c%2$csprintf(command, %3$cgcc -DREPLICATE %%1$s -o Sully_%%2$d%3$c, filename, i);%1$c%2$csystem(command);%1$c%2$cif (i > 0)%1$c%2$c{%1$c%2$c%2$csprintf(command, %3$c./Sully_%%d%3$c, i);%1$c%2$c%2$csystem(command);%1$c%2$c}%1$c%2$creturn 0;%1$c}%1$c";
 	fprintf(outfd, source, 10, 9, 34, source, i);
 	fclose(outfd);
 
 	char command[100];
-	sprintf(command, "gcc %1$s -o Sully_%2$d", filename, i);
+	sprintf(command, "gcc -DREPLICATE %1$s -o Sully_%2$d", filename, i);
 	system(command);
-	if (i)
+	if (i > 0)
 	{
 		sprintf(command, "./Sully_%d", i);
 		system(command);
